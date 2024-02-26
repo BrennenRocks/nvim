5b6e4e78ee8f752d7c34a2b2572716a0d23baec3
Brennen Davis <brennendavis@gmail.com>
2024-02-26 09:08:46 -0700

    use catppuccin theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  -- { "folke/tokyonight.nvim" },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "tokyonight-storm",
+  --   },
+  -- },
+  { "catppuccin/nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+}
