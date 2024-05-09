25fcc81daf8fc17abf53cde675b731ca366de8ce
Brennen Davis <brennendavis@gmail.com>
2024-05-09 10:02:22 -0600

    treesitter ensure_installed

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,18 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
+      "bash",
+      "css",
       "graphql",
+      "html",
+      "javascript",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
     },
   },
 }
