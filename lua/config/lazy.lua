3687f6f26b192110d5e32ade1a16e88b65ec37a0
Brennen Davis <brennendavis@gmail.com>
2024-05-20 16:39:34 -0600

    bring back context and mini surround

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
